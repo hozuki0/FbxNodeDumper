ef1888cc79157e3add548d3f2537915d1893f61c
hozukiMk3 <meta.maetel@gmail.com>
2019-07-12 12:17:09 +0900

    Submoduleを準備

---
+++ FbxWriter/FbxTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Fbx;
+
+namespace FbxTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//var document = FbxIO.ReadBinary(args[0]);
+			//FbxIO.WriteAscii(document, Path.GetDirectoryName(args[0]) + "/test_ascii.fbx");
+			var reader = new FbxAsciiReader(new FileStream(Path.GetDirectoryName(args[0]) + "/test_ascii.fbx", FileMode.Open));
+			var doc = reader.Read();
+			FbxIO.WriteAscii(doc, Path.GetDirectoryName(args[0]) + "/test_ascii_2.fbx");
+		}
+	}
+}
